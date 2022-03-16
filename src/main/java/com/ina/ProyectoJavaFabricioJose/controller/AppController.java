fd0fca91d078bb9582237f667cda399ee6238ce3
Jose0810 <joseandresvillalobos0@gmail.com>
2022-03-16 16:18:51 -0600

    Version 1.0 Desarrollo Jose, Paquetes controller, dao, domain, y services creados

---
+++ src/main/java/com/ina/ProyectoJavaFabricioJose/controller/AppController.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ina.ProyectoJavaFabricioJose.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+    @GetMapping("/")
+    public String inicio() {
+        return "index";
+    }
+}
