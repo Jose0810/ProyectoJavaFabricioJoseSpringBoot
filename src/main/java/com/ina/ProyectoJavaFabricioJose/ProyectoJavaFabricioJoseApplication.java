bc775f2b75ba47e7e97756628229c66227e4c228
Jose0810 <joseandresvillalobos0@gmail.com>
2022-03-16 14:58:31 -0600

    Version 1.0 Desarrollo Jose, Dependencias y cadena de conexión listas

---
+++ src/main/java/com/ina/ProyectoJavaFabricioJose/ProyectoJavaFabricioJoseApplication.java
@@ -0,0 +1,13 @@
+package com.ina.ProyectoJavaFabricioJose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoJavaFabricioJoseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoJavaFabricioJoseApplication.class, args);
+	}
+
+}
